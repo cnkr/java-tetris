f873f9c2d3fe65f04756b06683dcb0f0128cf22d
jenkinson26 <jenkinson26@gmail.com>
2018-10-21 16:22:30 -0500

    Free moving I block
    Now we have a grid, and an "I" piece which freely floats on the cells. Bounds check not implemented yet but the piece can be moved via WASD keys.

--- src/com/jenkinson/Game.java
+++ src/com/jenkinson/Game.java
@@ -2,90 +2,102 @@ package com.jenkinson;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 
-public class Game {
+public class Game extends JFrame implements Runnable {
 
     public static void main(String[] args) {
         new Game();
     }
 
-    //===========================
     // Fields
-    //===========================
 
     private enum State {RUNNING, STOPPED}
 
-    private JFrame frame;
+    private enum PieceKind {I, J, L, O, S, T, Z}
+
+    private enum Direction {LEFT, RIGHT, UP, DOWN}
+
     private GameBoard gameBoard;
     private State gameState;
 
-    private final int NUM_ROWS = 22;
+    private final int HIDDEN_ROWS = 4;
+    private final int NUM_ROWS = 20 + HIDDEN_ROWS;
     private final int NUM_COLS = 10;
     private final int SCALE = 40; // Number of pixels per cell
     private final int GAP = SCALE / 10; // Space in between cells
-    private final int STEP = SCALE + GAP;
+    private final int STEP = SCALE + GAP;  // Used to estimate cell pixel locations.
 
-    private final long FRAME_PERIOD = 250;
+    private final long MS_PER_FRAME = 1000/60;
 
     private Cell[][] grid;
 
-    //===========================
+    private Piece activePiece;
+    private Piece nextPiece;
+    private Color bgColor = new Color(0, 255, 255);
+
     // Methods
-    //===========================
 
     private Game() {
 
-        initGUI();
-        initGrid();
-        runGameLoop();
-    }
-
-    private void initGUI() {
-
-        frame = new JFrame("Tetris");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Tetris");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         gameBoard = new GameBoard();
         gameBoard.setPreferredSize(new Dimension(gameBoard.WIDTH, gameBoard.HEIGHT));
 
         JPanel boardWrapper = new JPanel();
         boardWrapper.add(gameBoard);
+        add(boardWrapper);
 
-        frame.add(boardWrapper);
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        addKeyListener(new ConcreteKeyListener());
+
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        createCellGrid();
+        createActivePiece();
+
+        Thread gameLoop = new Thread(this);
+        gameLoop.start();
     }
 
-    private void initGrid() {
-
+    private void createCellGrid() {
         grid = new Cell[NUM_ROWS][NUM_COLS];
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-
                 grid[i][j] = new Cell(i, j);
             }
         }
     }
 
-    private void runGameLoop() {
+    private void createActivePiece() {
+        activePiece = new Piece();
+    }
 
+    public void run() {
 
         gameState = State.RUNNING;
 
-        long tFirst, tLast, tSleep; // time
-        tFirst = System.currentTimeMillis();
+        long t0, t1, tSleep;
 
         while (gameState == State.RUNNING) {
+            t0 = System.currentTimeMillis();
 
-            update();
-            render();
+            // UPDATE
+            //activePiece.translatePiece(1, 1);
 
-            // Control Render Speed
-            tLast = System.currentTimeMillis();
-            tSleep = FRAME_PERIOD - (tLast - tFirst);
+            // RENDER
+            repaint();
+
+
+            // SLEEP
+            t1 = System.currentTimeMillis();
+            tSleep = MS_PER_FRAME - (t1 - t0);
 
             if (tSleep > 0) {
                 try {
@@ -94,8 +106,6 @@ public class Game {
                     e.printStackTrace();
                 }
             }
-
-            tFirst = tLast;
         }
     }
 
@@ -104,25 +114,117 @@ public class Game {
 
         Random r = new Random();
 
-        for(Cell[] row : grid){
-            for(Cell cell : row){
+        for (Cell[] row : grid) {
+            for (Cell cell : row) {
                 cell.color = new Color(r.nextInt(255), r.nextInt(255), r.nextInt(255));
             }
         }
 
     }
 
-    private void render() {
-        frame.repaint();
+    // Subclasses
+
+    class Block {
+        // Movable
+
+        int i;
+        int j;
+
+        Block(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
     }
 
-    //===========================
-    // Subclasses
-    //===========================
+    class Cell {
+        // Immovable
+
+        final int i;
+        final int j;
+        Color color;
+        boolean solid = false;
+
+        Cell(int i, int j) {
+            this.i = i;
+            this.j = j;
+
+            //Random rand = new Random();
+            //color = new Color(rand.nextInt(255), rand.nextInt(1), rand.nextInt(1));
+
+            color = bgColor;
+        }
+    }
+
+    class Piece {
+
+        PieceKind kind;
+        Color color;
+        Block[] blocks;
+
+        Piece() {
+            Random r = new Random();
+            PieceKind[] allValues = PieceKind.values();
+            PieceKind selectedKind = allValues[r.nextInt(allValues.length)];
+
+            selectedKind = PieceKind.I; //!!!
+
+            kind = selectedKind;
+
+            createBlocks();
+        }
+
+        void createBlocks() {
+            blocks = new Block[4];
+
+            switch (kind) {
+                case I:
+                    blocks[0] = new Block(0, 0);
+                    blocks[1] = new Block(1, 0);
+                    blocks[2] = new Block(2, 0);
+                    blocks[3] = new Block(3, 0);
+                    color = Color.BLUE;
+                    break;
+                case J:
+                    break;
+                case L:
+                    break;
+                case O:
+                    break;
+                case S:
+                    break;
+                case T:
+                    break;
+                case Z:
+                    break;
+                default:
+                    break;
+            }
+
+            translatePiece(0, 7);
+        }
+
+        void translatePiece(int x, int y) {
+
+            for (Block block : blocks) {
+                grid[block.i][block.j].solid = false;
+                grid[block.i][block.j].color = bgColor;
+            }
+
+            for (Block block : blocks) {
+                block.i += y;
+                block.j += x;
+                grid[block.i][block.j].solid = true;
+                grid[block.i][block.j].color = color;
+            }
+        }
+
+        void movePiece(Direction dir) {
+
+        }
+    }
 
     abstract class Board extends JPanel {
         // Abstract JFrame child to represent 3 main components of the game.
-
         int WIDTH;
         int HEIGHT;
     }
@@ -132,7 +234,7 @@ public class Game {
         GameBoard() {
 
             this.WIDTH = NUM_COLS * STEP - GAP;
-            this.HEIGHT = NUM_ROWS * STEP - GAP;
+            this.HEIGHT = (NUM_ROWS - HIDDEN_ROWS) * STEP - GAP;
         }
 
         protected void paintComponent(Graphics g) {
@@ -156,22 +258,37 @@ public class Game {
 
     }
 
-    class Cell {
+    class ConcreteKeyListener implements KeyListener {
 
-        final int i;
-        final int j;
-        Color color;
-
-        Cell(int i, int j) {
-            this.i = i;
-            this.j = j;
-
-            Random r = new Random();
-
-            color = new Color(r.nextInt(255), r.nextInt(255), r.nextInt(255));
+        @Override
+        public void keyTyped(KeyEvent e) {
+            System.out.println("Key typed.");
         }
 
-    }
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_A:
+                    activePiece.translatePiece(-1,0);
+                    break;
+                case KeyEvent.VK_D:
+                    activePiece.translatePiece(1,0);
+                    break;
+                case KeyEvent.VK_W:
+                    activePiece.translatePiece(0,-1);
+                    break;
+                case KeyEvent.VK_S:
+                    activePiece.translatePiece(0,1);
+                    break;
+                default:
+                    activePiece.translatePiece(1, 1);
+            }
+        }
 
+        @Override
+        public void keyReleased(KeyEvent e) {
+            System.out.println("Key released.");
+        }
+    }
 }
 
